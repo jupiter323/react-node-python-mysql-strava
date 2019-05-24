fd8a7931db5447ef22765df6d28c75ff597eb0b1
dreamhero418 <jupiter.fierce.31@gmail.com>
2019-05-24 06:28:11 -0400

    notifications string change remove blue and yello boxes

--- client/src/views/Pages/Notifications.jsx
+++ client/src/views/Pages/Notifications.jsx
@@ -114,7 +114,7 @@ class Notifications extends React.Component {
     return (
       <div className="marginTop">
         <Heading
-          title="Notifications"
+          title="Please open verification email send and click on link"
           textAlign="center"
 
           category={
@@ -133,7 +133,7 @@ class Notifications extends React.Component {
             </span>
           }
         />
-        <GridContainer justify="center">
+        {/* <GridContainer justify="center">
 
           <GridItem xs={12} sm={12} md={12} >
             <Card>
@@ -166,7 +166,7 @@ class Notifications extends React.Component {
               </GridContainer>
             </div>
           </GridItem>
-        </GridContainer>
+        </GridContainer> */}
       </div>
     );
   }
